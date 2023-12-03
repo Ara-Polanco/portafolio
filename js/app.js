77cd56b12c268f3bd16716f9710f06b12c618f17
Araceli Polanco <120612439+Ara-Polanco@users.noreply.github.com>
2023-12-03 01:08:49 -0600

    Hiding-showing: navigation menu

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 /* Ocultar y mostrar el menú de navegación */
+const nav = document.querySelector('#header__nav')
+const abrir = document.querySelector('#abrir')
+const cerrar = document.querySelector('#cerrar')
+
+abrir.addEventListener('click', () => {
+    nav.classList.add('visible')
+    abrir.classList.add('unvisible')
+})
+
+cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible')
+    abrir.classList.remove('unvisible')
+})
 
